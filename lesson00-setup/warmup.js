d100145b6bb11db7af20b710d8ac3bfe18d66e2e
Nima sherpa <Toplouie78@gmail.com>
2025-01-22 11:02:26 -0500

    warmup oo

--- lesson00-setup/warmup.js
+++ lesson00-setup/warmup.js
@@ -1,5 +1,9 @@
 // Write a function that introduces yourself. It should take your name and your hometown and return a string that says "Hello, my name is [your name] and I am from [your hometown]."
 
-function introduceYourself(name, hometown) {}
+function introduceYourself(name, hometown) {
+	console.log(`Hello, my name is ${name} and I am from ${hometown}.`);
+}
 
 // Then console.log the result of your function to the console.
+
+introduceYourself("Nima", "Nepal");
