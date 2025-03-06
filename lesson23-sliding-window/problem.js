61a306f687796f29717747ad32d5d9eabb469a48
Nima sherpa <Toplouie78@gmail.com>
2025-03-06 11:35:12 -0500

    sliding window 03/06/25

--- lesson23-sliding-window/problem.js
+++ lesson23-sliding-window/problem.js
@@ -11,7 +11,35 @@
 // Output: 3
 // Explanation: Any substring of length 3 contains exactly 3 distinct characters.
 
-module.exports = function longestSubstringKDistinct(str, k) {
-    // your code here
+const longestSubstringKDistinct = (str, k) => {
+	const distinctChar = {};
+	let max = -Infinity,
+		left = 0,
+		right = 0;
+
+	while (right < str.length) {
+		// add the key / counter to the hash map
+		distinctChar[str[right]] = (distinctChar[str[right]] || 0) + 1;
+		// unique number of char
+		let distinctNum = Object.keys(distinctChar).length;
+
+		// check if distinct char is apllicable
+		if (distinctNum <= k) {
+			max = Math.max(max, right - left + 1);
+			right++;
+		} else {
+			if (distinctChar[str[left]] === 1) {
+				delete distinctChar[str[left]];
+			} else {
+				distinctChar[str[left]] -= 1;
+			}
+			left++;
+		}
+	}
+
+	return max === -Infinity ? 0 : max;
 };
-  
+
+console.log(longestSubstringKDistinct("eceba", 2));
+console.log(longestSubstringKDistinct("aa", 1));
+console.log(longestSubstringKDistinct("abcdef", 3));
